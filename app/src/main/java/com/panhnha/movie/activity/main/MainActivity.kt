0f0623c467b27126e974d8cbcf2d412a1f0214d8
SotPanhnha <sotpanhnha@gmail.com>
2022-10-10 19:47:30 +0700

    version-01

---
+++ app/src/main/java/com/panhnha/movie/activity/main/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.panhnha.movie.activity.main
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.panhnha.movie.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/panhnha/movie/activity/main/fragment/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.panhnha.movie.activity.main.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
